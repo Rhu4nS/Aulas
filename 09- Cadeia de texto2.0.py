71bc3334499640768d5ab2bf66100093986a818b
Rhuan Santos <119426491+Rhu4nS@users.noreply.github.com>
2022-12-20 20:39:05 -0300

    Create 09- Cadeia de texto2.0.py

---
+++ 09- Cadeia de texto2.0.py
@@ -0,0 +1,31 @@
+# Manipulando texto
+
+frase = 'Curso em Video python'
+print(frase[3])
+Resultado: s
+
+
+frase = 'Curso em Video python'
+print(frase[3:13])
+Resultado: so em Vide
+
+
+frase = 'Curso em Video python'
+print(frase[1:15])
+Resultado: urso em Video 
+
+
+frase = 'Curso em Video python'
+print(frase[1:15:2])
+Resultado:us mVdo
+
+
+frase = 'Curso em Video python'
+print(frase[:15:2])
+Resultado: Croe ie 
+
+
+
+frase = 'Curso em Video python'
+print(frase[::2])
+Resultado: Croe ie yhn
