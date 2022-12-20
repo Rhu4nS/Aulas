8fea1d6be34b8e3b6856ffd02d86b908ab3e3d32
Rhuan Santos <119426491+Rhu4nS@users.noreply.github.com>
2022-12-20 18:52:46 -0300

    Create Operadores Aritimeticos.py

---
+++ Operadores Aritimeticos.py
@@ -0,0 +1,66 @@
+
+
+----------------------------------------------------
+Adição ( + )
+Divisão ( / )
+subtração ( - )
+Potencia ( ** )
+multiplicação ( * )
+Divisão inteira ( // )
+Resto da divisão ( % )
+----------------------------------------------------
+
+5 + 2 == 7
+5 / 2 == 2.5
+5 - 2 == 3
+5 ** 2 == 25
+5 * 2 == 10
+5 // 2 == 2
+5 % 2 == 1
+
+------------------------------------------------------
+ORDEM DE PRECEDÊNCIA
+
+1- ()
+2- **
+3-*, /, //, %
+4- +, -
+
+-------------------------------------------------------
+Concatenar:
+oi + olá = 'oiolá
+oi * 5 = oioioioioi
+
+'='*10 = ==========
+print('-')*10
+----------
+----------------------------------------------------------
+                    #FUNÇÃO PRINT
+# escrever a função em 20 caracteres:
+
+nome = input('Qual o seu nome ?')
+print('Prazer em te conhecer {:20}!'.format(nome)
+prazer em te conhecer Lucas                !
+
+-   -   -   -   -   -   -   -   -   -   -   -  -  -   -
+# Alinhar a função a direita em 20 caracteres
+
+nome = input('Qual o seu nome ?')
+print('Prazer em te conhecer {:>20}!'.format(nome)
+prazer em te conhecer           Lucas!
+
+-   -   -   -   -   -   -   -   -   -   -   -   -   -
+#Alinhar a função no centro em 20 caracteres
+
+nome = input('Qual o seu nome ?')
+print('Prazer em te conhecer {:^20}!'.format(nome)
+prazer em te conhecer     Lucas     !
+
+-   -   -   -   -   -   -   -   -   -   -   -   -   -  
+#Alinhar a função no centro com algum caracter especial em 20 espaços
+
+nome = input('Qual o seu nome ?')
+print('Prazer em te conhecer {:$^20}!'.format(nome)
+prazer em te conhecer $$$$$Lucas$$$$$!
+
+-   -   -   -   -   -   -   -   -   -   -   -   -   -  
